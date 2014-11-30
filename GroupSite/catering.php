53bfd4de239b70c70203b4d887e2fdc66570e740
Colleen Miller <colleen.miller6@yahoo.com>
2014-11-30 12:50:58 -0500

    catering

--- GroupSite/catering.php
+++ GroupSite/catering.php
@@ -8,25 +8,21 @@
      <link href="bootstrap.min.css" rel="stylesheet">
      <link href="style.css" rel="stylesheet">
 </head>
-<?php
-  $fName = basename(__FILE__);
-?>
 <body>
-	<?php include("header.php");
+	<?php 
+	$fName = basename(__FILE__);
+	include("header.php");
 	?>
-	<div class="catering">
-		<div class="col-xs-6">
-	<h1>Catering</h1>
-	<p>A. Regular(2-3 People): 20 pieces of sushi with 4 kinds of rolls</p>
-	<p>B. Deluxe(3-4 People): 30 pieces of sushi with 8 kinds of rolls</p>
-	<p>C. Special(4-6 People): 40 pieces of sushi with 8 kinds of rolls (1 special roll)</p>
-	<p>D. Supreme(6-8 People): 60 pieces of sushi with 10 kinds of rolls (2 special rolls)</p>
-	</div>
-		<div class="col-xs-6">
-			<img src="catering.png" alt="Picture of sushi" id="catering">
+	<div>
+		<div class="catering">
+			<h1>Catering</h1>
+			<p>A. Regular(2-3 People): 20 pieces of sushi with 4 kinds of rolls</p>
+			<p>B. Deluxe(3-4 People): 30 pieces of sushi with 8 kinds of rolls</p>
+			<p>C. Special(4-6 People): 40 pieces of sushi with 8 kinds of rolls (1 special roll)</p>
+			<p>D. Supreme(6-8 People): 60 pieces of sushi with 10 kinds of rolls (2 special rolls)</p>
 		</div>
+		<img id="cater" alt="Picture of catered sushi" src="catering.png">
 	</div>
-
 	<?php
 		include("footer.php");
 	?>
