5d67138e87364bfc317b670a6128b29778a5d726
Colleen Miller <colleen.miller6@yahoo.com>
2014-12-02 17:44:14 -0500

    catering page/fixed validate

--- GroupSite/catering.php
+++ GroupSite/catering.php
@@ -14,14 +14,18 @@
 	include("header.php");
 	?>
 	<div>
+		<div class="cater_pics">
+			<img id="cater" alt="Picture of catered sushi" src="catering.png">
+			<img id="other" alt="Another picture of catered sushi" src="cater.jpg">
+		</div>
 		<div class="catering">
 			<h1>Catering</h1>
-			<p>A. Regular(2-3 People): 20 pieces of sushi with 4 kinds of rolls</p>
-			<p>B. Deluxe(3-4 People): 30 pieces of sushi with 8 kinds of rolls</p>
-			<p>C. Special(4-6 People): 40 pieces of sushi with 8 kinds of rolls (1 special roll)</p>
-			<p>D. Supreme(6-8 People): 60 pieces of sushi with 10 kinds of rolls (2 special rolls)</p>
+			<p><b>A. Regular(2-3 People):</b> 20 pieces of sushi with 4 kinds of rolls</p>
+			<p><b>B. Deluxe(3-4 People):</b> 30 pieces of sushi with 8 kinds of rolls</p>
+			<p><b>C. Special(4-6 People):</b> 40 pieces of sushi with 8 kinds of rolls (1 special roll)</p>
+			<p><b>D. Supreme(6-8 People):</b> 60 pieces of sushi with 10 kinds of rolls (2 special rolls)</p>
+			<br><h4>*If you have further questions regarding catering, feel free to contact us!</h4>
 		</div>
-		<img id="cater" alt="Picture of catered sushi" src="catering.png">
 	</div>
 	<?php
 		include("footer.php");
--- GroupSite/validate.js
+++ GroupSite/validate.js
@@ -12,6 +12,9 @@ function checkZip(form){
 	if (document.getElementById('ZipCode').value == "48104"){
 		return true;
 	}
+	else if(document.getElementById('ZipCode').value == ""){
+		return true;
+	}
 	else {
 		alert("We do not deliver to your area");
 		return false;
