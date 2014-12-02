a0926bccc85ce603fb06523240adfa95f93df26d
Colleen Miller <colleen.miller6@yahoo.com>
2014-12-02 10:43:12 -0500

    home page

--- GroupSite/index.php
+++ GroupSite/index.php
@@ -14,9 +14,17 @@
 	$fName = basename(__FILE__);
 	include("header.php");
 	?>
-	<h1> SADAKO JAPANESE RESTAURANT</h1>
-	<h3>Ann Arbor's Fresh Choice for Fine Japanese Cuisine</h3>
-	<p>Are you looking for an interesting, classy atmosphere to take your family for lunch or dinner? Is your appetite looking for something different from that generic American restaurant down the street, with hamburgers that you’re tired of having? If any of this sounds familiar, then let us welcome you to Sadako Japanese Restaurant, where we offer a mouthwatering variety of traditional Japanese cuisine that you can definitely appreciate.</p>
+	<div class="col-xs-6">
+		<h1> SADAKO JAPANESE RESTAURANT</h1>
+		<h3>Ann Arbor's Fresh Choice for Fine Japanese Cuisine</h3>
+		<p>Are you looking for an interesting, classy atmosphere to take your family for lunch or dinner? Is your appetite looking for something different from that generic American restaurant down the street, with hamburgers that you’re tired of having? If any of this sounds familiar, then let us welcome you to Sadako Japanese Restaurant, where we offer a mouthwatering variety of traditional Japanese cuisine that you can definitely appreciate.</p>
+		<p>Feel free to explore our website to learn more about <b>Sadako Japanese Restaurant</b> and what we have to offer!</p>
+	</div>
+	<div class="col-xs-6">
+		<img src="store.jpg" alt="Sadako Store Front" id="store">
+	</div>
+	
+
 	<?php
 		include("footer.php");
 	?>
